d0f340288f2552e4b1094b50d36bf7cedd4a4063
Mohana Misra <iammohanamisra@gmail.com>
2023-12-23 23:54:56 +0530

    Added images for assurance section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import logo from './assets/logo.png'
+import img1 from './assets/img1.png'
+import img2 from './assets/img2.png'
+import img3 from './assets/img3.png'
+
 const App = () => {
     return (
         <div className = 'container'>
@@ -18,7 +22,11 @@ const App = () => {
             </div>
 
             <div className = 'assurance'>
-                <div className = 'images'></div>
+                <div className = 'images'>
+                    <div className = 'image-1'><img className = 'img1' src = {img1} alt = 'pride flag'/></div>
+                    <div className = 'image-2'><img className = 'img2' src = {img2} alt = 'men hugging'/></div>
+                    <div className = 'image-3'><img className = 'img3' src = {img3} alt = 'pride march under rainbow flag'/></div>
+                </div>
                 <div className = 'text'>
                     <div className = 'section-heading'></div>
                     <div className = 'section-text'></div>
